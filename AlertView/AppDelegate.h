7488104974faec90cb1def28bf8aeb08983a36c1
郝鹏飞 <haopf@rgnet.cn>
2015-12-09 15:53:25 +0800

    Initial Commit

---
+++ AlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertView
+//
+//  Created by 郝鹏飞 on 15/12/9.
+//  Copyright © 2015年 郝鹏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
